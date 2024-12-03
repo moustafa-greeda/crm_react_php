8436cab2bf80b0838c00eb154dc00b3ee7575277
moustafa-greeda <moustafagreeda@gmail.com>
2024-12-03 15:00:41 +0200

    tt

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -5,8 +5,6 @@ export default function Dashboard() {
     <div>
       <h1>Dashboard moustafa 133</h1>
       <h1>Dashboard ehab</h1>
-      <h1>Dashboard adam 23d </h1>
-      <h1>Dashboard adam 23d </h1>
 
       {/* will either be <Home/> or <Settings/> */}
       <Outlet />
