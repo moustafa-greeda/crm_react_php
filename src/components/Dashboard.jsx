6cd2e49b4d2f6572be80d13f560c2bbc48df99ab
Kelentooooon <ehapm5678@gmail.com>
2024-12-03 14:37:06 +0200

    ehab

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router";
 export default function Dashboard() {
   return (
     <div>
-      <h1>Dashboard moustafa</h1>
+      <h1>Dashboard ehabb</h1>
       {/* will either be <Home/> or <Settings/> */}
       <Outlet />
     </div>
