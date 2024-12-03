a73f76deb508bba25fbee90053600762242c389e
hend <khind3517@gmail.com>
2024-12-03 14:19:00 +0200

    hend1

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router";
 export default function Dashboard() {
   return (
     <div>
-      <h1>Dashboard hend</h1>
+      <h1>Dashboard hend 6</h1>
       {/* will either be <Home/> or <Settings/> */}
       <Outlet />
     </div>
