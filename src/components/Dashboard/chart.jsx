a8b379d59afbb676a0dd1cff8e49fc7c22f57d06
hind kamal <60649561+nodakamal18@users.noreply.github.com>
2024-12-10 14:54:57 +0200

    Added Dashboard component

---
+++ src/components/Dashboard/chart.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Bar } from "react-chartjs-2";
+
+const Chart = () => {
+  const data = {
+    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    datasets: [
+      {
+        label: "Dataset 1",
+        data: [50, 40, 60, 80, 30, 70, 90],
+        backgroundColor: "#333",
+      },
+      {
+        label: "Dataset 2",
+        data: [30, 60, 50, 70, 40, 60, 100],
+        backgroundColor: "#28a745",
+      },
+    ],
+  };
+
+  return (
+    <div style={{ marginTop: "20px" }}>
+      <Bar data={data} />
+    </div>
+  );
+};
+
+export default Chart;
