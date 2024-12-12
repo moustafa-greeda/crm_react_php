97814d0ffe51b46b4c42db430d363b62a5087047
Kelentooooon <ehapm5678@gmail.com>
2024-12-12 10:24:47 +0200

    .

--- src/App.js
+++ src/App.js
@@ -3,38 +3,27 @@ import Root from "./components/Root/Root";
 import Login from "./components/Auth/Login/Login";
 import Register from "./components/Auth/Register/Register";
 
-// import Dashboard from './components/Dashboard/Dashboard';
-import DragAndDropColumns from "./pages/Tasks/Tasks";
-import Settings from "./components/Settings/Settings";
-import { Dashboard } from "@mui/icons-material";
-// import Home from "./components/Home";
-import Users from "./components/Customer/User";
-import UserDashboard from "./components/User/UserDashboard";
-import Messages from "./components/Messages/Messages";
-
+import Dashboard from './components/Dashboard/Dashboard';
+ import Settings from './components/Settings/Settings';
+// import { Dashboard } from "@mui/icons-material";
+import Home from "./components/Home";
 function App() {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Root />,
-      children: [
-        { path: "/tasks", element: <DragAndDropColumns /> },
-        { path: "/dashboard", element: <Dashboard /> },
-        { path: "/user-dashboard", element: <UserDashboard /> },
-        { path: "/users", element: <Users /> },
-        { path: "/messages", element: <Messages /> },
-        { path: "/settings", element: <Settings /> }
-      ]
-    },
-    {
-      path: "/register",
-      element: <Register />
-    },
-    {
-      index: true,
-      element: <Login />
-    }
-  ]);
+  const router =createHashRouter([{
+    path:'/',element:<Root/>,children:[
+      {path:"home",element:<Home/>},
+      // {path:"/dashboard",element:<Dashboard/>},
+      {path:"/settings",element:<Settings/>}
+    ]    
+},
+{
+  path: '/login',
+  element: <Login />,
+},
+{
+  index:true,
+  element: <Register />,
+},
+])
   return (
     <>
       <RouterProvider router={router}></RouterProvider>
